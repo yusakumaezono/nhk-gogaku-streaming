8f6b247c20e05eca4d4cd3080a7d9fac41dd9f3c
Maezono Yusaku <yusakumaezono@gmail.com>
2013-09-15 21:22:13 +0900

    アイコンを変更

--- GogakuForNHK/define.h
+++ GogakuForNHK/define.h
@@ -1,3 +1,3 @@
-#define GAI_TRACKING_ID @"UA-782100-9"
-#define FEEDBACK_MAIL_ADDRESS @"maezono@crossgate.bz"
+#define GAI_TRACKING_ID @""
+#define FEEDBACK_MAIL_ADDRESS @""
 #define SUPPORT_URL @"http://crossgate.bz/?dt_portfolio=gogaku-for-nhk"
